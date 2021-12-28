98a264ca5ee18ec8f59388740a22b97b3fa8d123
Prince <princessant89@gmail.com>
2021-12-28 15:15:41 +0530

    changed background colour

---
+++ lib/Screens/front.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Frontpage extends StatelessWidget {
+  const Frontpage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF81D4FA),
+    );
+  }
+}
