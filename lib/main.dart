0a637e5d0f841d63108f27c109cf27b97866b3aa
Kumkum-IIT <kwatrakumkum02@gmail.com>
2021-12-28 21:10:28 +0530

    change color to white

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
+    show BuildContext, Key, MaterialApp, StatelessWidget, Widget, runApp;
 import 'package:self_help/Screens/front.dart';
 
-void main(){
+void main() {
   runApp(SelfHelp());
 }
 
 class SelfHelp extends StatelessWidget {
-  const SelfHelp({ Key? key }) : super(key: key);
+  const SelfHelp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
