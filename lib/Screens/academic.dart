b8bb4b56bd5287b54eb702e9061c1060d9cfac7c
Prince <princessant89@gmail.com>
2021-12-30 20:24:59 +0530

    added academic page

---
+++ lib/Screens/academic.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class academic_page extends StatelessWidget {
+  const academic_page({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
